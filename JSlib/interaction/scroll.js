8894a25b373f4b1a8c0a944775db09cf3382bf81
Jizhou-Che <che_mail@icloud.com>
2021-05-30 19:40:53 +0800

    Tangent scrolling function implemented.

--- JSlib/interaction/scroll.js
+++ JSlib/interaction/scroll.js
@@ -1,3 +1,15 @@
 function scroll_windowScrollPercent() {
 	return window.scrollY / (document.body.offsetHeight - window.innerHeight);
 }
+
+function scroll_linear(scroll, start, stop) {
+	return (scroll - start) / (stop - start);
+}
+
+function scroll_tangent(scroll, start, stop) {
+	k = 0.9;
+	x = ((scroll - start) / (stop - start)) * (k * Math.PI) - (k * Math.PI / 2);
+	shiftY = Math.tan(k * Math.PI / 2);
+	scaleY = shiftY * 2;
+	return (Math.tan(x) + shiftY) / scaleY;
+}
--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 window.onload = function() {
+	// Define content scroll dictionary.
+	contentScrollDic = {
+		content_1: [0.05, 0.5]
+	}
+	
 	// Set up scroll event listener for window.
 	testScroll = document.querySelector("#test_scroll");
 	window.onscroll = function() {
-		testScroll.innerHTML = "Scroll: " + scroll_windowScrollPercent() + ".";
+		scroll = scroll_windowScrollPercent();
+		testScroll.innerHTML = "Scroll: " + scroll + ".";
+		renderContent(contentScrollDic, scroll);
 	}
 	
 	// Set up animation for hiding renovation notice.
@@ -17,4 +24,17 @@ function renderHideRenovations(progress) {
 	renovations = document.querySelector("#renovations");
 	height = renovations.offsetHeight;
 	renovations.style.marginTop = (0 - height * progress) + "px";
+}
+
+function renderContent(contentScrollDic, scroll) {
+	for (contentID in contentScrollDic) {
+		content = document.querySelector("#" + contentID);
+		if (scroll >= contentScrollDic[contentID][0] && scroll <= contentScrollDic[contentID][1]) {
+			contentScrollPercent = scroll_tangent(scroll, contentScrollDic[contentID][0], contentScrollDic[contentID][1]);
+			content.style.top = (window.innerHeight - (window.innerHeight + content.offsetHeight) * contentScrollPercent) + "px";
+			content.style.opacity = 1 - Math.abs(2 * contentScrollPercent - 1);
+		} else {
+			content.style.top = "100%";
+		}
+	}
 }
